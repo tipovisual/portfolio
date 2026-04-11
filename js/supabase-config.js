d2209fb10c7e0964dd40619a86b12fe523be67b1
tipovisual <emoemulio@gmail.com>
2026-04-11 14:08:45 -0500

    fix: corrección final de llaves y permisos supabase

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://qhiflgztlcrbedmkhmmi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoaWZsZ3p0bGNyYmVkbWtobW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4ODU5NDAsImV4cCI6MjA5MTQ2MTk0MH0.g3J-K-uTGnYUi8fwKlnz4A75aMQrj2rrujuqjWKPFw8N_KEY'
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoaWZsZ3p0bGNyYmVkbWtobW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4ODU5NDAsImV4cCI6MjA5MTQ2MTk0MH0.g3J-K-uTGnYUi8fwKlnz4A75aMQrj2rrujuqjWKPFw8'
 export const supabase = createClient(supabaseUrl, supabaseKey);
